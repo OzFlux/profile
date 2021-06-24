513028f747dfb142ca100556a7153e9333df7d99
Ian McHugh <imchugh@2200l-154691-l.mobility.unimelb.net.au>
2021-06-24 15:43:17 +1000

    Fixed import functions to deal with mangled timestamps in raw data

--- WombatStateForest.py
+++ WombatStateForest.py
@@ -8,30 +8,47 @@ Created on Mon Mar 16 14:14:25 2020
 
 import numpy as np
 import pandas as pd
-
+import pdb
 import profile_utils as pu
 
 #-----------------------------------------------------------------------------
 ### Variables ###
 #-----------------------------------------------------------------------------
 
-#-----------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 irga_drop_list = [['2011-11-09 12:00', '2012-03-04 15:00'],
                   ['2012-11-10 13:00:00', '2013-03-04 16:00:00'],
                   ['2014-07-16 12:00', '2014-12-10 09:00:00'],
-                  ['2015-10-06 03:00:00', '2020-02-03 08:00:00']]
+                  ['2015-10-06 03:00:00', '2019-11-05 11:54:00']]
+irga_baseline_offset_dict = {'first_offset': 
+                             {'start_date': '2019-11-05 12:00:00',
+                              'end_date': '2020-01-29 10:00:00',
+                              'offset': 300}}
 irga_vars_to_import=['TIMESTAMP', 'Cc_LI840_1m', 'Cc_LI840_2m', 'Cc_LI840_4m',
                      'Cc_LI840_8m', 'Cc_LI840_15m', 'Cc_LI840_30m']
 met_drop_list = [['2014-05-21 13:00:00', '2014-06-18 16:30:00']]
 met_vars_to_import=['TIMESTAMP', 'Ta_HMP_02_Avg', 'ps_7500_Avg']
-#-----------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
-#-----------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 ### Functions ###
 #------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------
+def adjust_CO2_data(df, adjust_dict):
+
+    add_df = pd.DataFrame(data=0, index=df.index, columns=df.columns)
+    for offset in adjust_dict:
+        inner_dict = adjust_dict[offset]
+        add_df.loc[inner_dict['start_date']: inner_dict['end_date']] = (
+            inner_dict['offset']
+            )
+    return df + add_df
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
 def drop_data(df, drop_list):
+
     for date_pair in drop_list:
         df.loc[date_pair[0]: date_pair[1]] = np.nan
     return df
@@ -49,7 +66,13 @@ def get_heights(columns):
     return [int(x.split('_')[-1].replace('m', '')) for x in columns]
 #------------------------------------------------------------------------------
 
+#------------------------------------------------------------------------------
+def merge_Tair_data(df):
+    
+    pass
+#------------------------------------------------------------------------------
 
+#------------------------------------------------------------------------------
 def resample_data(df):
 
     return df[np.mod(df.index.minute, 30) < 4].resample('30T').mean()
@@ -79,15 +102,16 @@ def get_data():
     irga_path = pu.get_path(state='raw', series='profile', 
                             site='WombatStateForest')
     co2_series = (
-        pu.open_data(file_dir=irga_path, search_str='IRGA',
+        pu.open_data(file_dir=irga_path, search_str='IRGA_avg',
                      vars_to_import=irga_vars_to_import,
                      freq='2T')
         .pipe(resample_data)
         .pipe(drop_data, irga_drop_list)
+        .pipe(adjust_CO2_data, irga_baseline_offset_dict)
         .pipe(filter_data, [300, 600])
         .pipe(stack_to_series, 'CO2')
                  )
-
+    
     # Construct temp and pressure df
     met_path = pu.get_path(state='raw', series='flux_slow', 
                            site='WombatStateForest', check_exists=True)
@@ -118,19 +142,4 @@ def get_data():
 
     # Create xarray dataset
     return pd.concat([co2_series, ta_series, ps_series], axis=1).to_xarray()
-#------------------------------------------------------------------------------
-
-#------------------------------------------------------------------------------
-### GLOBALS ###
-#------------------------------------------------------------------------------
-
-#------------------------------------------------------------------------------
-irga_drop_list = [['2011-11-09 12:00', '2012-03-04 15:00'],
-                  ['2012-11-10 13:00:00', '2013-03-04 16:00:00'],
-                  ['2014-07-16 12:00', '2014-12-10 09:00:00'],
-                  ['2015-10-06 03:00:00', '2020-02-03 08:00:00']]
-irga_vars_to_import=['TIMESTAMP', 'Cc_LI840_1m', 'Cc_LI840_2m', 'Cc_LI840_4m',
-                     'Cc_LI840_8m', 'Cc_LI840_15m', 'Cc_LI840_30m']
-met_drop_list = [['2014-05-21 13:00:00', '2014-06-18 16:30:00']]
-met_vars_to_import=['TIMESTAMP', 'Ta_HMP_02_Avg', 'ps_7500_Avg']
 #------------------------------------------------------------------------------
--- profile_utils.py
+++ profile_utils.py
@@ -6,6 +6,8 @@ Created on Tue May 18 17:47:40 2021
 """
 
 from configparser import ConfigParser
+import datetime as dt
+import logging
 import pandas as pd
 import pathlib
 import pdb
@@ -15,7 +17,6 @@ def get_path(state, series, site, check_exists=False):
     
     state_dict = {'raw': 'raw_data_read_path', 
                   'processed': 'processed_data_write_path'}
-    # series_list = ['slow_flux', 'fast_flux', 'profile']
     proc_type = state_dict[state]
     config = ConfigParser()
     config.read(pathlib.Path(__file__).parent / 'paths.ini')
@@ -26,9 +27,9 @@ def get_path(state, series, site, check_exists=False):
 #-----------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------
-def open_data(file_dir, search_str, vars_to_import, freq):
-
-    file_list = list(file_dir.glob('*{}*.dat'.format(search_str)))
+def open_data(file_dir, search_str, freq, vars_to_import=None, start_year=None):
+    
+    file_list = list(file_dir.rglob('*{}*.dat'.format(search_str)))
     if  not file_list: 
         raise FileNotFoundError('No files found containing searchphrase {}'
                                   .format(search_str))
@@ -37,14 +38,25 @@ def open_data(file_dir, search_str, vars_to_import, freq):
         try:
             df_list.append(
                 pd.read_csv(f, parse_dates=['TIMESTAMP'], skiprows=[0, 2, 3],
-                            index_col=['TIMESTAMP'], usecols=vars_to_import,
-                            na_values='NAN', error_bad_lines=False,
-                            dtype={x: 'float' for x in vars_to_import}))
+                            index_col=['TIMESTAMP'], na_values='NAN', 
+                            error_bad_lines=False))
         except ValueError:
             continue
     return_df = pd.concat(df_list)
+    return_df = return_df[~return_df.index.isnull()]
+    if start_year: 
+        return_df = return_df[return_df.index.year >= start_year]
+    if vars_to_import:
+        valid_list = [x for x in vars_to_import if x in return_df.columns]
+        invalid_list = [x for x in vars_to_import if not x in return_df.columns]
+        if invalid_list:
+            logging.warning('The following values were missing from the '
+                            'concatenated data and will be ignored: {}'
+                            .format(', '.join(invalid_list)))
+        return_df = return_df[valid_list]
     return_df.sort_index(inplace = True)
     return_df = return_df[~return_df.index.duplicated(keep = 'first')]
+    return_df = return_df[~return_df.index.isnull()]
     new_index = pd.date_range(return_df.index[0], return_df.index[-1], freq=freq)
     return return_df.reindex(new_index)
 #------------------------------------------------------------------------------
